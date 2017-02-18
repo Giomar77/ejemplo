db80434021dee5542345cd7889a47f12def943c0
Giomar77 <baney.acosta@udea.edu.co>
2017-02-17 19:45:27 -0500

    Version 3.0

---
+++ src/example/Example.java
@@ -0,0 +1,17 @@
+
+package example;
+
+//UNIVERSIDAD DE ANTIOQUIA.
+
+public class Example {
+
+public static void main(String[] args){
+    
+    grettings gr= new grettings();
+    
+    gr.Saludo();
+    
+
+    }
+}
+
